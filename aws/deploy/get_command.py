d747b34f64fb005e8f3ce8d8de5eb88389adf668
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-03-28 00:07:50 -0300

    -r

--- aws/deploy/get_command.py
+++ aws/deploy/get_command.py
@@ -18,7 +18,7 @@ def get_command(profile: str, region: str, jenkins: str, command_id: str):
 
 
 status_replace = lambda status, erase_len, simbol="": print(
-    f'\r{" " * erase_len}\r{simbol} {status}', end="", flush=True
+    f'\r{" " * erase_len + len(simbol)}\r{simbol}{status}', end="", flush=True
 )
 
 
@@ -28,7 +28,7 @@ def get_and_wait(profile: str, region: str, jenkins: str, command_id: str):
     print(status, end="")
     while status == "InProgress":
         for simbol in ["⣾", "⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽"]:
-            status_replace(status, len(status) + 2, simbol)
+            status_replace(status, len(status), simbol + " ")
             sleep(1)
         y = len(status) + 2
         status = get_status()
