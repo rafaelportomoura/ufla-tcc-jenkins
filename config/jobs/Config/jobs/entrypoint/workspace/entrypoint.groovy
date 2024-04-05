af0179024855ac730c1a7e91004e522a3ae0056b
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-05 06:30:57 -0300

    Fix git credential helper command

--- config/jobs/Config/jobs/entrypoint/workspace/entrypoint.groovy
+++ config/jobs/Config/jobs/entrypoint/workspace/entrypoint.groovy
@@ -63,7 +63,7 @@ job('Config/nodejs') {
 job('Config/git') {
     steps {
         sh("""
-            git config --global credential.helper '!aws codecommit credential-helper $@'
+            git config --global credential.helper '!aws codecommit credential-helper \$@'
             git config --global credential.UseHttpPath true
         """)
     }
