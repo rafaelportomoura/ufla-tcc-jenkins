0de5604867978569acce736aabaedd197d0d0314
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-14 20:05:40 -0300

    Refactor Jenkins pipeline configuration and scripts

--- pipelines/tcc/entrypoint.groovy
+++ pipelines/tcc/entrypoint.groovy
@@ -1,7 +1,57 @@
-// Carrega o script de utilidade
-def createJob = evaluate(new File("pipelines/shared/createJob.groovy"))
-def jenkins_repo = "https://git-codecommit.us-east-2.amazonaws.com/v1/repos/ufla-tcc-jenkins";
-// Utiliza a função do script carregado
-createJob("tcc/", "dev", jenkins_repo, "H/05 * * * *", "main", "echo 'Hello, World!'", {
-    string('CLEANUP', 'false', 'Clean up the workspace?')
-})
+String pipelines_path = 'pipelines/tcc'
+
+def scmConfig(
+    String repo = 'https://git-codecommit.us-east-2.amazonaws.com/v1/repos/ufla-tcc-jenkins',
+    String branch='main'
+) {
+    scm {
+        remote {
+            url(repo)
+        }
+        branch('main')
+    }
+}
+
+def triggerConfig() {
+    triggers {
+        scm('H/05 * * * *')
+    }
+}
+
+def publisherConfig() {
+    publishers {
+        cleanWs {
+            cleanWhenAborted(true)
+            cleanWhenFailure(true)
+            cleanWhenNotBuilt(false)
+            cleanWhenSuccess(true)
+            cleanWhenUnstable(true)
+            deleteDirs(true)
+            notFailBuild(true)
+            disableDeferredWipeout(true)
+            patterns {
+                pattern {
+                    type('EXCLUDE')
+                    pattern('.propsfile')
+                }
+                pattern {
+                    type('INCLUDE')
+                    pattern('.gitignore')
+                }
+            }
+        }
+    }
+}
+
+job('Config/nodejs') {
+    triggerConfig()
+    scmConfig()
+    steps {
+        dsl {
+            external("${pipelines_path}/dev.groovy")
+            removeAction('DELETE')
+            removeViewAction('DELETE')
+        }
+    }
+    publisherConfig()
+}
