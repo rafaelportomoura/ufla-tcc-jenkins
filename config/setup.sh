83d47db2c8add484821ae5e76dc1ee1c8c09ca17
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-14 23:07:27 -0300

    Update setup.sh to install additional dependencies and Python 3.10.12

--- config/setup.sh
+++ config/setup.sh
@@ -12,7 +12,9 @@ sudo dnf install -y --quiet java-17-amazon-corretto-devel \
   unzip \
   python3-pip \
   python3-devel \
-  kernel-devel
+  kernel-devel \
+  bzip2-devel \
+  libffi-devel
 sudo dnf groupinstall "Development Tools" -y --quiet
 echo "INSTALLED DEPENDENCIES"
 sudo systemctl daemon-reload
@@ -24,3 +26,14 @@ git config --global --add safe.directory $repository
 sudo chmod 777 --recursive $repository/.git
 sudo chown -R jenkins:jenkins $repository
 sudo echo \"fim do script de setup\" >>/var/chegou_ao_fim.txt
+
+sudo cd /tmp
+sudo wget --quiet https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
+sudo tar -xzf Python-3.10.12.tgz
+cd Python-3.10.12
+./configure --enable-optimizations --prefix=/usr/local
+make -j 2
+sudo make altinstall
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.10 get-pip.py
+sudo echo \"fim do script de python\" >>/var/chegou_ao_fim.txt
