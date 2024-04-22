b1780c6c3a8e2dc9550f9734eaeb0f789902c371
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-22 20:55:06 -0300

    Refactor package declaration in prod.groovy and infra.groovy

--- pipelines/tcc/prod.groovy
+++ pipelines/tcc/prod.groovy
@@ -27,4 +27,4 @@ Infra.job(this, job_folder, "vpc", env_disable_pipes, codecommit, default_branch
 Infra.job(this, job_folder, "network", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_version, "create_network.py")
 Infra.job(this, job_folder, "domain", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_version, "create_domain.py")
 Infra.job(this, job_folder, "certificate", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_version, "create_certificate.py")
-Infra.job(this, job_folder, "package-bucket", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_version, "create_bucket.py")
+Infra.job(this, job_folder, "package-bucket", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_version, "create_package_bucket.py")
