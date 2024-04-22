c5c69ab328e3c40203e7124d3b119cf7790a79d7
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-22 20:30:54 -0300

    Refactor variable name in prod.groovy to use JENKINS_HOME environment variable

--- pipelines/tcc/prod.groovy
+++ pipelines/tcc/prod.groovy
@@ -1,8 +1,8 @@
 System.getenv().each { key, value ->
     println("${key} = ${value}")
 }
-String workspace = System.getenv('WORKSPACE')
-def modules = load("${workspace}/pipelines/libs/infra.groovy")
+String workspace = System.getenv('JENKINS_HOME')
+def modules = load("${workspace}/workspace/entrypoints/tcc-prod/pipelines/libs/infra.groovy")
 
 // ACCOUNT PARAMETERS
 String profile="default"
