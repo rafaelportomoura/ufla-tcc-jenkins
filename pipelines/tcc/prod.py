480f620b7a8034d169bc2550285e71a515ab44f3
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-23 01:59:25 -0300

    Refactor file path in prod.py to use prod_dir instead of pipe_dir

--- pipelines/tcc/prod.py
+++ pipelines/tcc/prod.py
@@ -6,10 +6,6 @@ pipe_dir = os.path.dirname(prod_dir)
 libs_dir = os.path.join(pipe_dir, "libs")
 
 output_file = ""
-with open(os.path.join(prod_dir, "prodheader.groovy"), "r") as f:
-    output_file += f.read()
-output_file += "\n"
-
 libs_files = os.listdir(libs_dir)
 
 for lib_file in libs_files:
@@ -17,6 +13,10 @@ for lib_file in libs_files:
         with open(os.path.join(libs_dir, lib_file), "r") as f:
             output_file += f.read()
             output_file += "\n"
+
+with open(os.path.join(prod_dir, "prodheader.groovy"), "r") as f:
+    output_file += f.read()
+output_file += "\n"
 
 with open(os.path.join(prod_dir, "prod.groovy"), "r") as f:
     output_file += f.read()
