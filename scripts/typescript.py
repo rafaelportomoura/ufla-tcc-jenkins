0b28e217ad84622efe2fd4b773e192fe9d3c2466
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-23 02:29:58 -0300

    Refactor Typescript class in typescript.py to include node_version parameter and update build method

--- scripts/typescript.py
+++ scripts/typescript.py
@@ -4,6 +4,7 @@ import os
 class Typescript:
     def __init__(
         self,
+        node_version: int = 20,
         package_manager: str = "pnpm",
         package_manager_install: str = "install",
         package_manager_dev_build_flags: list[str] = [
@@ -19,6 +20,7 @@ class Typescript:
             "--ignore-scripts",
         ],
     ):
+        self.node_version = node_version
         self.package_manager = package_manager
         self.package_manager_install = package_manager_install
         self.package_manager_dev_build_flags = " ".join(package_manager_dev_build_flags)
@@ -29,12 +31,15 @@ class Typescript:
     def build(self, cmd: str = "build") -> None:
         package_install = f"{self.package_manager} {self.package_manager_install}"
         dev_install = f"{package_install} {self.package_manager_dev_build_flags}"
-        os.system(dev_install)
-        os.system("find ./node_modules -mtime +10950 -exec touch {} +")
-        os.system(f"{self.package_manager} run {cmd}")
-        os.system("rm -rf node_modules")
-        prod_install = f"{package_install} {self.package_manager_prod_build_flags}"
-        os.system(prod_install)
+        os.system(
+            f". ~/nvm/nvm.sh \
+                  && nvm use {self.node_version} \
+                  && {dev_install} \
+                  && find ./node_modules -mtime +10950 -exec touch {{}} \
+                  && {self.package_manager} run {cmd} \
+                  && rm -rf node_modules \
+                  && {package_install} {self.package_manager_prod_build_flags}"
+        )
 
     def lint(self, cmd: str = "lint") -> None:
         os.system(f"{self.package_manager} run {cmd}")
