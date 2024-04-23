4d4f295edd226819d5f060fa0f33aabbea75865d
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-23 18:56:52 -0300

    Refactor Docker class in docker.py to use build_and_push method

--- scripts/docker.py
+++ scripts/docker.py
@@ -1,19 +1,18 @@
-import os 
+import os
+
 
 class Docker:
-    def __init__(self, ecr_uri: str, image: str, tag: str):
-        self.ecr_uri = ecr_uri
-        self.image = image
-        self.tag = tag
-    
-    def build(self) -> None:
-        os.system(f"docker build -t {self.image}:{self.tag} .")
+    @staticmethod
+    def build_and_push(
+        ecr_uri: str, image: str, tag: str, region: str, profile: str = None
+    ) -> None:
+        profile = f"--profile {profile}" if profile and profile != "default" else ""
+        os.system(
+            f"docker build -t {ecr_uri}/{image}:{tag} \
+                  && aws {profile} --region {region} ecr get-login-password | get docker login --username AWS --password-stdin {ecr_uri} \
+                  && docker push {ecr_uri}/{image}:{tag}"
+        )
 
-    def push(self) -> None:
-        os.system(f"docker tag {self.image}:{self.tag} {self.ecr_uri}/{self.image}:{self.tag}")
-        os.system(f"aws ecr get-login-password | get docker login --username AWS --password-stdin {self.ecr_uri}")
-        os.system(f"docker push {self.ecr_uri}/{self.image}:{self.tag}")
-        
     @staticmethod
     def ecr_uri(account_id: str, region: str) -> str:
         return f"{account_id}.dkr.ecr.{region}.amazonaws.com"
