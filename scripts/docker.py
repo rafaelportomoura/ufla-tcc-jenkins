e4b2ee62d29812d4f8092d491806daa2e64ff171
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 20:10:29 -0300

    fix: docker build

--- scripts/docker.py
+++ scripts/docker.py
@@ -8,9 +8,9 @@ class Docker:
     ) -> None:
         profile = f"--profile {profile}" if profile and profile != "default" else ""
         os.system(
-            f"echo '🐋 Building image...' && docker build -t {ecr_uri}/{image}:{tag} . > /dev/null\
-                  && aws {profile} --region {region} ecr get-login-password |  docker login --username AWS --password-stdin {ecr_uri} > /dev/null\
-                  echo '🐋 Push image' && docker push {ecr_uri}/{image}:{tag} > /dev/null"
+            f"echo '🐋 Building image...' && docker build -t {ecr_uri}/{image}:{tag} . > /dev/null &&\
+                aws {profile} --region {region} ecr get-login-password | docker login --username AWS --password-stdin {ecr_uri} > /dev/null &&\
+                echo '🐋 Push image' && docker push {ecr_uri}/{image}:{tag} > /dev/null"
         )
 
     @staticmethod
