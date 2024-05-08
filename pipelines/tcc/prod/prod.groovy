10aa1a58eca1f57dd960ef5040a2fd51b4ef950b
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 18:00:23 -0300

    Update job to create us-east-1 certificate if region is not us-east-1

--- pipelines/tcc/prod/prod.groovy
+++ pipelines/tcc/prod/prod.groovy
@@ -7,7 +7,7 @@ Infra.job(this, job_folder, "network", env_disable_pipes, codecommit, default_br
 Infra.job(this, job_folder, "domain", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_exe, "create_domain.py")
 Infra.job(this, job_folder, "certificate", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_exe, "create_certificate.py")
 if (region != 'us-east-1') {
-    Infra.job(this, job_folder, "us-east-1-certificate", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, 'us-east-1', profile, python_exe, "create_us_east_1_certificate.py")
+    Infra.job(this, job_folder, "us-east-1-certificate", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_exe, "create_us_east_1_certificate.py")
 }
 Infra.job(this, job_folder, "package-bucket", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_exe, "create_package_bucket.py")
 Infra.job(this, job_folder, "kms", env_disable_pipes, codecommit, default_branch, jenkins_scripts, stage, tenant, region, profile, python_exe, "create_kms.py")
