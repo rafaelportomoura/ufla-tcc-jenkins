6bf674ef485f672980d071a8fc97a25ff61c8bbe
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 20:14:16 -0300

    refactor: Fix docker login command in docker.py script

--- scripts/docker.py
+++ scripts/docker.py
@@ -9,7 +9,7 @@ class Docker:
         profile = f"--profile {profile}" if profile and profile != "default" else ""
         os.system(
             f"echo '🐋 Building image...' && docker build -t {ecr_uri}/{image}:{tag} . > /dev/null &&\
-                aws {profile} --region {region} ecr get-login-password | docker login --username AWS --password-stdin {ecr_uri} &&\
+                aws {profile} --region {region} ecr get-login-password | docker login --username AWS --password-stdin {ecr_uri} > /dev/null &&\
                 echo '🐋 Push image' && docker push {ecr_uri}/{image}:{tag} > /dev/null"
         )
 
