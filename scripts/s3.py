49e0bb1671227b80c222d20a101de0607642a95d
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 22:20:20 -0300

    refactor: Fix AWS command in remove_from_bucket function

--- scripts/s3.py
+++ scripts/s3.py
@@ -3,7 +3,7 @@ from scripts.log import Log
 
 
 def remove_from_bucket(bucket: str, log: Log, profile: str = None) -> None:
-    cmd += "aws"
+    cmd = "aws"
     cmd += f" --profile {profile}" if profile or profile != "default" else ""
     cmd += f" s3 rm s3://{bucket} --recursive"
     log.cmd(cmd)
