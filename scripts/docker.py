d92cac7cb09861b6afee50f33799a36466706f62
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-09 12:30:37 -0300

    refactor: Update Docker class push_image method to use `cli_read.pre_defined_cmd` for command execution

--- scripts/docker.py
+++ scripts/docker.py
@@ -14,11 +14,9 @@ class Docker:
         profile = f"--profile {profile}" if profile and profile != "default" else ""
         full_image = f"{ecr_uri}/{image}:{tag}"
         self.log.info(f"🐋 Building image {full_image}")
-        output = self.cli_read.pre_defined_cmd(
-            f"docker build -t {full_image} . --quiet"
-        )
+        output = os.popen(f"docker build -t {full_image} . --quiet")
         self.log.info(output)
-        self.cli_read.pre_defined_cmd(
+        os.popen(
             " | ".join(
                 [
                     f"aws {profile} --region {region} ecr get-login-password",
@@ -27,7 +25,7 @@ class Docker:
             )
         )
         self.log.info(f"🐋 Push image {full_image}")
-        output = self.cli_read.pre_defined_cmd(f"docker push {full_image}")
+        output = os.popen(f"docker push {full_image}")
         self.log.info(output)
 
     def ecr_uri(self, account_id: str, region: str) -> str:
