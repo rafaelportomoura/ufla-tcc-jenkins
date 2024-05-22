690138ebd34b23ff32b288e724ab23913077343d
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-22 15:29:45 -0300

    refactor(scripts): add NotFoundException class

--- scripts/exception.py
+++ scripts/exception.py
@@ -5,3 +5,9 @@ class DeployException(Exception):
     def __init__(self, stack: Stack):
         self.message = "Failed to deploy stack: " + stack.stack_name
         super().__init__(self.message)
+
+
+class NotFoundException(Exception):
+    def __init__(self, message: str):
+        self.message = f"❌ {message}"
+        super().__init__(self.message)
