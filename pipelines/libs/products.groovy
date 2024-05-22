b8f5f3d0eabf2aea09e4dd3d12da5fd128f15c74
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-22 18:02:30 -0300

    fix pipes

--- pipelines/libs/products.groovy
+++ pipelines/libs/products.groovy
@@ -9,7 +9,7 @@ class Products {
       }
       blockOnUpstreamProjects()
       properties {
-        priority(12
+        priority(12)
       }
       parameters{
           choiceParam('LogLevel',log_levels, 'Select compute services log level')
